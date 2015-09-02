cf693cbbfada4863bde3f5f6ce10cf7c784fb805
Ilya Gorbachevsky <igorbachevsky@gmail.com>
2015-09-02 08:14:24 +0300

    Improve UX for account register

--- client/scripts/auth/controllers/register-ctrl.controller.js
+++ client/scripts/auth/controllers/register-ctrl.controller.js
@@ -10,11 +10,62 @@ module.exports = function ( app ) {
   function controller( User, $state, socket ) {
     var vm = this;
     vm.controllername = fullname;
+    vm.authFail = false;
+    vm.errors = {
+      email: {
+      },
+      username: {
+      },
+      password: {
+      }
+    };
 
     vm.register = function () {
       User.create(vm.user, function ( resource, headersGetter ) {
-        vm.login();
-      });
+          vm.authFail = false;
+          vm.login();
+        },
+        function ( response ) {
+          var error = response.data.error;
+          var errorCodes = error.details.codes;
+          var errorMessages = error.details.messages;
+          vm.authFail = true;
+
+          if ( errorCodes.email ) {
+            vm.errors.email.e = {};
+            vm.errors.email.m = {};
+            vm.errors.email.v = vm.user.email;
+
+            for ( var i = 0; i < errorCodes.email.length; i++ ) {
+              vm.errors.email.e[errorCodes.email[i]] = true;
+              vm.errors.email.m[errorCodes.email[i]] = errorMessages.email[i];
+            }
+          }
+
+          if ( errorCodes.username ) {
+            vm.errors.username.e = {};
+            vm.errors.username.m = {};
+            vm.errors.username.v = vm.user.username;
+
+            for ( var i = 0; i < errorCodes.username.length; i++ ) {
+              vm.errors.username.e[errorCodes.username[i]] = true;
+              vm.errors.username.m[errorCodes.username[i]] = errorMessages.username[i];
+            }
+          }
+
+          if ( errorCodes.password ) {
+            vm.errors.password.e = {};
+            vm.errors.password.m = {};
+            vm.errors.password.v = vm.user.password;
+
+            for ( var i = 0; i < errorCodes.password.length; i++ ) {
+              vm.errors.password.e[errorCodes.password[i]] = true;
+              vm.errors.password.m[errorCodes.password[i]] = errorMessages.password[i];
+            }
+          }
+
+          console.log(vm.errors);
+        });
     };
 
     vm.login = function () {
